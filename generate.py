c8126e852822d334e2eb5c31c9a0ae1b4ff0e221
Hermite Bai <hermitebai@outlook.com>
2017-04-16 19:21:37 -0500

    finish generate.py

--- generate.py
+++ generate.py
@@ -4,10 +4,10 @@ import os
 from PIL import Image
 
 opt = {
-  model : 'models/golf/iter65000_net.t7',
-  batchSize : 128,
-  gpu : 1,
-  cudnn : 1,
+  'model' : 'models/golf/iter65000_net.t7',
+  'batchSize' : 128,
+  'gpu' : 1,
+  'cudnn' : 1,
 }
 
 train = 'false'
@@ -21,28 +21,23 @@ importer = tf.train.import_meta_graph(opt['model'])
 importer.restore(sess, tf.train.latest_checkpoint('./'))
 
 noise = tf.random_normal((opt['batchSize'], 100))
-#TODO: Run model to generate videos
+gen = sess.run(y, feed_dict={x: noise})
+video = sess.run(h4)[0]
+mask = sess.run(h4)[1]
+static = sess.run(h4)[2]
+mask = tf.tile(mask, tf.constant([1,3,1,1,1]))
 
 
-gen = 
-video = 
-mask = 
-static = 
-mask =
-
-
-def WriteGif(filename, movie) {
-	for i in range(1, movie.shape[3]):
-		#TODO: save generated imgs
-	cmd = "ffmpeg -f image2 -i " + filename + ".%08d.png -y " + filename
+def WriteGif(filename, movie):
+	for i in range(1, movie.shape[2]):
+		Image.fromarray(movie.eval()[3]).save()
+	cmd = 'ffmpeg -f image2 -i ' + filename + '.%08d.png -y ' + filename
 	print('==> ' + cmd)
 	os.system(cmd)
-	for i in range(1, movie.shape[3]):
-		#TODO: delete imgs
+	for i in range(1, movie.shape[2]):
+		os.remove(filename + '.%08d.png'.format(i))
 
-}
-
-if not os.path.exists("vis/"):
+if not os.path.exists('vis/'):
 	os.mkdir('vis/')
 WriteGif('vis/gen.gif', gen)
 WriteGif('vis/video.gif', video)
